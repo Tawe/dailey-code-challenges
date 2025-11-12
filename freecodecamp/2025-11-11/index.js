26d012a7e27d282ebfbc63292083f5a070e3926b
John Munn <johnmunn5@gmail.com>
2025-11-12 13:37:55 -0400

    update challenges

--- freecodecamp/2025-11-11/index.js
+++ freecodecamp/2025-11-11/index.js
@@ -1,6 +1,17 @@
 function count(str) {
+    const vowels = ["a","e","i","o","u"]
+    const count = [0, 0];
+   
+    for (const ch of str.toLowerCase()) {
+        if (!/[a-z]/.test(ch)) continue; // skip anything that's not a letter
+        if (vowels.includes(ch)) count[0]++;
+        else count[1]++;
+    }
 
-  return str;
+    return count;
 }
 
 const x = count("Hello World")
+x;
+
+
