55463ff1c5ce3cc4fa9357be99026d4c00db8663
John Munn <munn.jon@gmail.com>
2026-01-28 13:52:55 -0400

    Daily Challenge

---
+++ freecodecamp/2026/01/2026-01-28/index.py
@@ -0,0 +1,10 @@
+def flatten(arr):
+    result = []
+    for item in arr:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+print(flatten([1, [2, [3, [4]]]]))
